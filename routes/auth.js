a6c4a793695f7f15f14e1383634d6c7754cba935
saksham yadav <sakyadav9936@gmail.com>
2025-09-01 00:38:09 +0530

    Switched from bcrypt to bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authCtrl = require("../controllers/authController");
 
-const bcrypt = require("bcryptjs");
 
 router.post("/register", authCtrl.register);
 router.post("/login", authCtrl.login);
