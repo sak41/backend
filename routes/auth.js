bf52031d2606131a83fa7244e5aa85bcb3333bf3
saksham yadav <sakyadav9936@gmail.com>
2025-08-31 17:56:02 +0530

    Descriptive message about the changes

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const authCtrl = require("../controllers/authController");
 
+const bcrypt = require("bcryptjs");
+
 router.post("/register", authCtrl.register);
 router.post("/login", authCtrl.login);
 
